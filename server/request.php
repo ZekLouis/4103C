464e833682bc87c457df13d0a74089f951140aed
EtiennePericat <etienne.pericat.etu@mail.com>
2017-01-27 11:10:33 +0100

    ecriture lecture JSOOOOOOOOOOOOOOOOOON mon fwraire

--- server/request.php
+++ server/request.php
@@ -7,6 +7,9 @@ header('Content-Type: application/json');
 
 // Le type de la requete sera défini par $_GET['no_req']
 
+$tableauJoueur = array("Nbjoueur" => 0, "liste" => array("joueur1" => "tata", "joueur2" => "titi"));
+
+
 switch($_GET['no_req']){
     
     case 0:
@@ -17,9 +20,18 @@ switch($_GET['no_req']){
         echo json_encode('Récupérer le contenu du tableau 2');
         break;
 
-    
-    default:
+    //inscrire nouveau joueur dans fichier JSON
+    case 3:
+    	file_put_contents('data.json', json_encode($tableauJoueur));
         break;
+
+    case 4:
+    	$json = json_decode(file_get_contents('data.json'));
+
+    	foreach ($json->liste as $joueur) {
+	    	echo $joueur;
+		}
+
 }
 
 ?>
