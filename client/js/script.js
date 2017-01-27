7b25e6e88b72520c229560cfdce3a10f3a2b2306
Louis Gaume <louis.gaume@etu.unilim.fr>
2017-01-27 11:59:22 +0100

    Design tableau

--- client/js/script.js
+++ client/js/script.js
@@ -2,11 +2,29 @@
  * Created by zeklouis on 23/01/17.
  */
 
+// Requete 0 : récupération des données 
+
 $(function(){
     setInterval(function(){
-        $.getJSON("/4103C/server/request.php?no_req=0",function(data){
+        var xhr = new XMLHttpRequest();
+        /*$.getJSON("/4103C/server/request.php?no_req=0",function(data){
             console.info(data[1]);
             console.info(data[2]);
+        });*/
+
+        xhr.open('GET', '/4103C/server/request.php?no_req=0');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr.status == 200){
+                console.log(JSON.parse(xhr.responseText)[1])
+            }else if(xhr.readyState == 4 && xhr.status != 200){
+                console.log('erreur')
+            }
+        }
+
+        $(".btn").click(function(){
+            $(this).addClass("red");
         });
     },1000);
    
