3da8e41db74104b16788a6f34415c2303368e14b
titi36 <etienne.pericat.etu@gmail.com>
2017-03-07 18:43:10 +0100

    ajout gestion des tour au clic

--- client/js/script.js
+++ client/js/script.js
@@ -132,13 +132,12 @@ $(function(){
         $.getJSON("/4103C/server/request.php?no_req=0&nomPartie="+nomPartie,function(data){
             console.info(data);
             if (data['pseudotour'] == pseudo){
-              jouer = true;
               //modifs d'affichages
+              $(".btnAdv").removeClass("disabled");
             }
             else{
-              jouer = false;
               //modifs d'affichages
-
+              $(".btnAdv").addClass("disabled");
             }
 
             if (data['nbJoueurs'] == 2 ){
@@ -146,7 +145,7 @@ $(function(){
             }
 
         });
-    },1000);
+    },100);
 
 /*  Placement des bateaux */
     var heightTab = 10;
